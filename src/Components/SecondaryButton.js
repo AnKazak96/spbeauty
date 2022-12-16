52f8ccae9650b05ca73eb2c186a3671bec537f49
AnKazak96 <120014531+AnKazak96@users.noreply.github.com>
2022-12-16 14:36:34 +0000

    Внесла изменение в Secondary

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,15 +3,16 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth;
     const Button = styled.div`
         background: #FFFFFF;
+        border-radius: 24px;
         border: 1px solid #64748B;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #FFFFFF;
+            border: 1px solid #ffffff;
         }
         &:active {
             background: #FFFFFF;
